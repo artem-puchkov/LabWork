31b5b6a9123ff31fe03e7b43d9b2ebbd8327655e
artem-puchkov <104691728+artem-puchkov@users.noreply.github.com>
2023-11-10 14:58:06 +0300

    refactoring

--- src/LabWork18/Task4.java
+++ src/LabWork18/Task4.java
@@ -6,9 +6,9 @@ public class Task4 {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
         System.out.print("Enter an integer ");
-        String intString = myScanner.next();
-        int i = Integer.parseInt(intString);
         try {
+            String intString = myScanner.next();
+            int i = Integer.parseInt(intString);
             System.out.println(2 / i);
         } catch (Exception e) {
             System.out.println("ArithmeticException was thrown");
